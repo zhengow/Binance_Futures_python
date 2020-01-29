3bba3bd4308d89b1e99fc52f2ccece75b8501a62
xkommando <xkommando@outlook.com>
2020-01-28 22:57:21 -0600

    fix model problem

--- binance_f/model/balance.py
+++ binance_f/model/balance.py
@@ -2,6 +2,7 @@ class Balance:
 
     def __init__(self):
         self.asset = ""
+        self.accountAlias = ""
         self.balance = 0.0
         self.withdrawAvailable = 0.0
 
@@ -9,6 +10,7 @@ class Balance:
     def json_parse(json_data):
         result = Balance()
         result.asset = json_data.get_string("asset")
+        result.accountAlias = json_data.get_string("accountAlias")
         result.balance = json_data.get_float("balance")
         result.withdrawAvailable = json_data.get_float("withdrawAvailable")
 
