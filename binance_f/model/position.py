5bbabb5e6e0c1ad7309ea3c818f1b87310f2575a
Mathieu Renauld <cornoualis@gmail.com>
2020-04-10 20:09:32 +0200

    'positionSide' was missing in 'Position' definition

--- binance_f/model/position.py
+++ binance_f/model/position.py
@@ -12,6 +12,8 @@ class Position:
         self.unrealizedProfit = 0.0
         self.marginType = ""
         self.isolatedMargin = 0.0
+        self.positionSide = ""
+
 
     @staticmethod
     def json_parse(json_data):
@@ -27,4 +29,5 @@ class Position:
         result.unrealizedProfit = json_data.get_float("unRealizedProfit")
         result.marginType = json_data.get_string("marginType")
         result.isolatedMargin = json_data.get_float("isolatedMargin")
+        result.positionSide = json_data.get_string("positionSide")
         return result
