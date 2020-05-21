3a4bd025fc880ec143b0942b21f3f22fe07b1d98
李桐 <tong.li@binance.com>
2020-05-21 16:40:04 +0800

    update base url for both prod and testnet

--- binance_f/constant/system.py
+++ binance_f/constant/system.py
@@ -1,9 +1,11 @@
 
 class WebSocketDefine:
     Uri = "wss://stream.binancefuture.com/ws"
+#    Uri = "wss://fstream.binance.com/ws"
 
 class RestApiDefine:
     Url = "https://testnet.binancefuture.com"
+#    Url = "https://fapi.binance.com"
 
 
 
