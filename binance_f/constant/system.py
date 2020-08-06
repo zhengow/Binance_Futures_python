a1c7bf9e5bc49faa472ed866e8acf99c3eb416a9
Rishabh Kapoor <RishabhKapoor99@gmail.com>
2020-08-06 18:58:30 +0530

    Update system.py

--- binance_f/constant/system.py
+++ binance_f/constant/system.py
@@ -1,7 +1,7 @@
 
 class WebSocketDefine:
-    Uri = "wss://stream.binancefuture.com/ws"
-#    Uri = "wss://fstream.binance.com/ws"
+#    Uri = "wss://stream.binancefuture.com/ws"
+    Uri = "wss://fstream.binance.com/ws"
 
 class RestApiDefine:
 #     Url = "https://testnet.binancefuture.com"
